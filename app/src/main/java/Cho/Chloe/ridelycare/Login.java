483693b79a5e21613cbed43253c23905f7d7d399
whdbstj <choevie080317@gmail.com>
2023-02-06 16:46:16 +0900

    Login

---
+++ app/src/main/java/Cho/Chloe/ridelycare/Login.java
@@ -0,0 +1,13 @@
+package Cho.Chloe.ridelycare;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.login);}
+}
